6f27c9cc4e54db6b4399dd30a7731f3b6b602cdc
pavan kumar dixit <pavandixit775@gmail.com>
2026-02-02 18:35:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello CI/CD Pipeline");
 });
 
 app.listen(3000, () => {
   console.log("App running on port 3000");
 });
+
