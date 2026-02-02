8fd0211bb639f91e005f9f897db3bc77cd6ddf2b
pavandixit02 <pavandixit775@gmail.com>
2026-02-02 21:01:22 +0530

    fix: bind express app to all network interfaces

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("Hello CI/CD Pipeline");
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("App running on port 3000");
 });
 
