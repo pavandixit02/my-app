7c1f27cbad5fcd9f7bfbb54c4b68d29aa57642c0
pavandixit02 <pavandixit775@gmail.com>
2026-02-03 10:19:59 +0000

    updae app.js

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD Pipeline");
+  res.send("Hello CI/CD Pipeline\n");
+});
+
+app.get("/", (req,res) =>{
+  res.send("this is demo of CID piepline working work application");
 });
 
 app.listen(3000, "0.0.0.0", () => {
